a92bfbab6e0d4342d6188dbfcdbe5595fe7fc6b7
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-08-18 20:34:07 +0100

    Add the detail page

--- src/app/components/event-detail.js
+++ src/app/components/event-detail.js
@@ -1,8 +1,49 @@
 import React from "react";
-import { getAllEvents } from "../../../dummy-data";
+import AddressIcon from "./icons/address-icon";
+import DateIcon from "./icons/date-icon";
+import styles from "./event-detail.module.css";
 
-const EventDetail = () => {
-  return <div>{getAllEvents.map(deta)}</div>;
+const EventDetail = (props) => {
+  const { detail } = props;
+
+  const readableDate = new Date(detail?.date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formatAddress = detail?.location.replace(", ", "\n");
+
+  return (
+    <>
+      <div className={styles.summary}>
+        <h1>{detail?.title}</h1>
+      </div>
+      <div className={styles.logistics}>
+        <div className={styles.image}>
+          {/* <img src={detail?.image} alt="{detail?.title}" /> */}
+          <img src={"/" + detail?.image} alt={detail?.title} />
+        </div>
+        <div className={styles.list}>
+          <div className={styles.item}>
+            <span className={styles.icon}>
+              <DateIcon />
+            </span>
+
+            <time>{readableDate}</time>
+          </div>
+          <div className={styles.item}>
+            <span className={styles.icon}>
+              <AddressIcon />
+            </span>
+            <address>{formatAddress}</address>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.content}>{detail?.description}</div>
+    </>
+  );
 };
 
 export default EventDetail;
--- src/app/events/[eventId]/page.js
+++ src/app/events/[eventId]/page.js
@@ -1,7 +1,18 @@
+"use client";
 import React from "react";
+import { getEventById } from "../../../../dummy-data";
+import EventDetail from "@/app/components/event-detail";
 
-const EventDetails = () => {
-  return <div>Event Details</div>;
+const EventDetails = (props) => {
+  const { params } = props;
+  const eventId = params.eventId;
+  console.log(eventId);
+  if (!eventId) {
+    return <p>Event not found.</p>;
+  }
+  const event = getEventById(eventId);
+
+  return <EventDetail detail={event} />;
 };
 
 export default EventDetails;
