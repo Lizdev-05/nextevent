bbd4fedbb631dffde8095c630cb14a42d85aaef7
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-08-19 17:12:33 +0100

    Handle the go back home

--- src/app/events/[eventId]/page.js
+++ src/app/events/[eventId]/page.js
@@ -2,13 +2,22 @@
 import React from "react";
 import { getEventById } from "../../../../dummy-data";
 import EventDetail from "@/app/components/events/event-detail";
+import ResultsTitle from "@/app/components/ui/results-title/results-title";
+import ErrorAlert from "@/app/components/ui/error-alert/error-alert";
 
 const EventDetails = (props) => {
   const { params } = props;
   const eventId = params.eventId;
   console.log(eventId);
   if (!eventId) {
-    return <p>Event not found.</p>;
+    return (
+      <>
+        <ErrorAlert>
+          <p>Event not found.</p>;
+        </ErrorAlert>
+        <ResultsTitle />
+      </>
+    );
   }
   const event = getEventById(eventId);
 
