154663b075ad40f0e32cd9b5610d1b3c93301298
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-08-19 16:53:16 +0100

    Routed to the slug page

--- src/app/events/[...slug]/page.js
+++ src/app/events/[...slug]/page.js
@@ -1,6 +1,45 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { getFilteredEvents } from "../../../../dummy-data";
 
-const FilteredEventPage = () => {
+const FilteredEventPage = (props) => {
+  const { params } = props;
+  const filteredData = params.sluf;
+  // const router = useRouter();
+  // const filteredData = router.query.page;
+
+  if (!filteredData) {
+    return <p className="center">Loading...</p>;
+  }
+  const filteredYear = filteredData[0];
+  const filteredMonth = filteredData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <p className="center">Invalid Filter. Kindly adjust your input...</p>
+    );
+  }
+
+  const filteredEvent = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvent || filteredEvent === 0) {
+    return <p className="center">No event found for the chosen filter</p>;
+  }
+
   return <div>Filtered Event</div>;
 };
 
